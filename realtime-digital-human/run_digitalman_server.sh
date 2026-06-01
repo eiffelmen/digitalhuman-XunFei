abbc651a081bc636b824be93e0c27bcf9ff8dfcd
wangzhen <wangzhen@Mac.lan>
2026-06-01 16:03:40 +0800

    Load backend env in realtime startup

--- realtime-digital-human/run_digitalman_server.sh
+++ realtime-digital-human/run_digitalman_server.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-export LLM_PROVIDER=${LLM_PROVIDER:-gongan}
+set -e
+
+cd "$(dirname "$0")"
+
+if [ -f .env ]; then
+    set -a
+    # shellcheck disable=SC1091
+    . ./.env
+    set +a
+fi
+
+export LLM_PROVIDER=${LLM_PROVIDER:-iflytek}
 export LISTEN_PORT=${LISTEN_PORT:-8010}
-export TTS_TYPE=${TTS_TYPE:-sparktts}
+export TTS_TYPE=${TTS_TYPE:-iflytts}
 export TTS_SERVER=${TTS_SERVER:-http://localhost:8779}
-# export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-1}
+export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0}
 
 .venv/bin/python app_v2.py --max_session 10 --tts "$TTS_TYPE" --TTS_SERVER "$TTS_SERVER" --wav2lip_size 256 --transport webrtc
